08224a224b8c6399be28f8a918d4adbe030cd553
Konrad <kondykox@gmail.com>
2025-02-01 11:56:03 +0100

    replaced json with constants

---
+++ src/constants/images.ts
@@ -0,0 +1,15 @@
+export const IMAGES = [
+    {
+      "src": "olga_buzi.jpg",
+      "alt": "Buzi buzi"
+    },
+    {
+      "src": "olga_mvp.jpg",
+      "alt": "MVP"
+    },
+    {
+      "src": "olga_energol.jpg",
+      "alt": "Buzi z energetykiem"
+    }
+  ];
+  
